51746636d37e1c568ffc2d88e3d65037b928374d
fateh491989 <fss491989@gmail.com>
2019-12-24 18:41:30 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // We will use UID here and on scannning we will perform searching;
+  // Uss any scanner tool to  scan
+  final data = "UID12334 ";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            QrImage(
+              data: data,
+              gapless: true,
+              size: 100,
+              errorCorrectionLevel: QrErrorCorrectLevel.H,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
