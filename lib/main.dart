0b599edfce16ee82c68f385d1fc7bf8254b75985
fateh491989 <fss491989@gmail.com>
2019-12-24 18:54:27 +0530

    Adding QR Scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
   // We will use UID here and on scannning we will perform searching;
   // Uss any scanner tool to  scan
   final data = "UID12334 ";
-
+  String result = 'No data';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,10 +43,44 @@ class _MyHomePageState extends State<MyHomePage> {
               gapless: true,
               size: 100,
               errorCorrectionLevel: QrErrorCorrectLevel.H,
-            )
+            ),
+            RaisedButton(onPressed: scanQR),
+            Text(result)
           ],
         ),
       ),
     );
   }
+
+
+  startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+        "#ff6666", "Cancel", true, ScanMode.BARCODE)
+        .listen((barcode) => print(barcode));
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.QR);
+      print(barcodeScanRes);
+      
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      result = barcodeScanRes;
+    });
+  }
+
+
+
 }
